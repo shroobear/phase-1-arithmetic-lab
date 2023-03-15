ef502737a89cd839fbf0fa32b895f050c65c10d5
Stanley Shrewsbery <homestanrunner@gmail.com>
2023-03-15 09:15:13 -0400

    Lab complete

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+const multiply = (num1 * num2);
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 24
+let num4 = 5
+const mod = (num3 % num4)
+
+const max = (Math.max(20, 4, 7, 16, 12))
